4f6f1f48ed66b227e824bf7fa856e94121978dab
Jamie Kelly <git@jamiekelly.com>
2023-03-05 08:37:25 +0000

    Initial basic VPC
    Includes public, private, and nat subnets.
    Each subnet is auto-generated based on given VPC CIDR block.
    Suitable defaults for my purposes.

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  total_subnets = ((var.enable_public_subnets ? 1 : 0) + (var.enable_nat_subnets ? 1 : 0) + (var.enable_private_subnets ? 1 : 0)) * var.number_of_availability_zones
+  subnet_newbits = ceil(log(local.total_subnets, 2))
+}
---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  for_each = {
+    for index, az_name in 
+      slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
+      : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index)
+      if var.enable_public_subnets
+  }
+
+  vpc_id = aws_vpc.main.id
+  availability_zone = each.key
+  cidr_block = each.value
+}
+
+resource "aws_subnet" "nat" {
+  for_each = {
+    for index, az_name in 
+      slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
+      : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index + var.number_of_availability_zones)
+      if var.enable_nat_subnets
+  }
+
+  vpc_id = aws_vpc.main.id
+  availability_zone = each.key
+  cidr_block = each.value
+}
+
+resource "aws_subnet" "private" {
+  for_each = {
+    for index, az_name in 
+      slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
+      : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index + (2 * var.number_of_availability_zones))
+      if var.enable_private_subnets
+  }
+
+  vpc_id = aws_vpc.main.id
+  availability_zone = each.key
+  cidr_block = each.value
+}
