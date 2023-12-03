7dd9ceca39d1e83d06a211c8f18fcd6360816c8b
Jamie Kelly <git@jamiekelly.com>
2023-12-02 19:48:52 -0800

    Add Internet Gateway when using a public subnet

--- main.tf
+++ main.tf
@@ -2,43 +2,77 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "public" {
-  for_each = {
-    for index, az_name in 
-      slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
-      : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index)
-      if var.enable_public_subnets
-  }
+resource "aws_internet_gateway" "main" {
+  count = var.enable_public_subnets ? 1 : 0
 
   vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  for_each = {
+    for index, az_name in
+    slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
+    : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index)
+    if var.enable_public_subnets
+  }
+
+  vpc_id            = aws_vpc.main.id
   availability_zone = each.key
-  cidr_block = each.value
+  cidr_block        = each.value
 
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "nat" {
-  for_each = {
-    for index, az_name in 
-      slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
-      : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index + var.number_of_availability_zones)
-      if var.enable_nat_subnets
-  }
+output "public_subnet_ids" {
+  value = values(aws_subnet.private)[*].id
+  description = "Public Subnet IDs"
+}
+
+resource "aws_route_table" "public" {
+  count = var.enable_public_subnets ? 1 : 0
 
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main[0].id
+  }
+
+  route {
+    cidr_block = aws_vpc.main.cidr_block
+    gateway_id = "local"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = var.enable_public_subnets ? aws_subnet.public : {}
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public[0].id
+}
+
+resource "aws_subnet" "nat" {
+  for_each = {
+    for index, az_name in
+    slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
+    : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index + var.number_of_availability_zones)
+    if var.enable_nat_subnets
+  }
+
+  vpc_id            = aws_vpc.main.id
   availability_zone = each.key
-  cidr_block = each.value
+  cidr_block        = each.value
 }
 
 resource "aws_subnet" "private" {
   for_each = {
-    for index, az_name in 
-      slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
-      : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index + (2 * var.number_of_availability_zones))
-      if var.enable_private_subnets
+    for index, az_name in
+    slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones)
+    : az_name => cidrsubnet(aws_vpc.main.cidr_block, local.subnet_newbits, index + (2 * var.number_of_availability_zones))
+    if var.enable_private_subnets
   }
 
-  vpc_id = aws_vpc.main.id
+  vpc_id            = aws_vpc.main.id
   availability_zone = each.key
-  cidr_block = each.value
+  cidr_block        = each.value
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "vpc_cidr" {
-  type = string
+  type        = string
   description = "IPv4 CIDR block for the VPC"
-  default ="10.0.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "number_of_availability_zones" {
-  type = number
+  type        = number
   description = "Number of availabilitiy zones to use"
-  default = 2
+  default     = 2
 }
 
 variable "enable_public_subnets" {
-  type = bool
+  type        = bool
   description = "Whether to create public subnets"
-  default = false
+  default     = false
 }
 
 variable "enable_nat_subnets" {
-  type = bool
+  type        = bool
   description = "Whether to create nat subnets"
-  default = false
+  default     = false
 }
 
 variable "enable_private_subnets" {
-  type = bool
+  type        = bool
   description = "Whether to create private subnets"
-  default = false
+  default     = false
 }
